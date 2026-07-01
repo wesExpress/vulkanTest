2d4b11081dee273f3b3e532cf1585183531d4dae
wes <wespeters18@gmail.com>
2026-07-01 13:17:00 -0400

    testing compute pipeline creation

--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -1,7 +1,6 @@
 #version 460
 
 #extension GL_EXT_buffer_reference : require
-#extension GL_EXT_shader_explicit_arithmetic_types_int64 : require
 #extension GL_EXT_descriptor_heap : require
 #extension GL_EXT_nonuniform_qualifier : require
 
